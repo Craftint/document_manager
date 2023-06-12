26a46c5b58c6733f21d068904e135b4de8fe8030
Aslam Kaithakath <aslamkaithakath@gmail.com>
2023-06-12 19:23:30 +0000

    doctype assigning

---
+++ document_manager/document_manager/doctype/document_master/test_document_master.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Dx and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDocumentMaster(FrappeTestCase):
+	pass
---
+++ document_manager/document_manager/doctype/document_settings/document_settings.js
@@ -0,0 +1,5 @@
+frappe.ui.form.on('Document Settings', {
+	refresh(frm){
+
+	}
+});
